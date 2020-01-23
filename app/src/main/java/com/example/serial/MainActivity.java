700dad6d205a35fa93773358de5d6042bb4ffaee
hsur753 <Hussel=2000>
2020-01-24 11:44:32 +1300

    arduino code

--- app/src/main/java/com/example/serial/MainActivity.java
+++ app/src/main/java/com/example/serial/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.serial;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -14,6 +15,9 @@ import android.hardware.usb.UsbEndpoint;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,17 +25,25 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.LineNumberReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.charset.StandardCharsets;
 import java.sql.Time;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -50,10 +62,42 @@ public class MainActivity extends Activity {
     private InputStream inStream;
     private OutputStream mOutputStream;
     private Thread readSerialDataThread;
+    public static final int MESSAGE_READ=0;
+
     String message = "";
+    String m = "";
     private boolean shouldRun = true;
+    boolean isDollar = false;
+
+    UsbSerialPort port;
 
     private String command = "0";
+
+    @SuppressLint("HandlerLeak")
+    Handler mHandler=new Handler()
+    {
+        @Override
+        public void handleMessage(Message msg_type) {
+            super.handleMessage(msg_type);
+
+            switch (msg_type.what){
+                case MESSAGE_READ:
+
+                    byte[] readbuf=(byte[])msg_type.obj;
+                    int i;
+                    try {
+
+                        String string_recieved=new String(readbuf);
+
+                        if ((char) readbuf[0] == '$' && (char) readbuf[11] == '&') {
+                            serialMessageHandler(readbuf.clone());
+                        }
+                    }
+                    catch (Exception e){Log.e("", "Could't read incoming message");}
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +108,7 @@ public class MainActivity extends Activity {
         sendButton = findViewById(R.id.button);
 
         context = getApplicationContext();
+        /*
 
         // Find all available drivers from attached devices.
 
@@ -102,7 +147,7 @@ public class MainActivity extends Activity {
 
                 }
             }
-        });
+        }); */
 
         //TODO: Better to implement displaying text to UI this way
         /*runOnUiThread(new Runnable() {
@@ -116,49 +161,125 @@ public class MainActivity extends Activity {
 
         });*/
 
+        // Find all available drivers from attached devices.
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        List<UsbSerialDriver> availableDrivers = UsbSerialProber.getDefaultProber().findAllDrivers(manager);
+        if (availableDrivers.isEmpty()) {
+            return;
+        }
+
+        // Open a connection to the first available driver.
+        UsbSerialDriver driver = availableDrivers.get(0);
+        UsbDeviceConnection connection = manager.openDevice(driver.getDevice());
+        if (connection == null) {
+            // add UsbManager.requestPermission(driver.getDevice(), ..) handling here
+            return;
+        }
+
+        try{
+        port = driver.getPorts().get(0); // Most devices have just one port (port 0)
+        port.open(connection);
+        port.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+        ConnectThread connectThread = new ConnectThread();
+        connectThread.start();
+        }
+        catch (Exception e){
+            Toast toast = Toast.makeText(context, "XXXXXXXXXXX", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!editText.getText().toString().equals("")) {
+                    String data = editText.getText().toString();
+                    try {
+                        byte[] outBuf = data.getBytes();
+                        port.write(outBuf, 0);
+
+                        //Toast toast = Toast.makeText(context, "Sent to Arduino", Toast.LENGTH_SHORT);
+                        //toast.show();
+
+                    } catch (Exception e) {Log.e("","Couldn't Write"); e.printStackTrace();}
+
+                }
+            }
+        });
+
+    }
+
+    private class ConnectThread extends Thread{
+        public void run(){
+            byte[] response;
+            int len;
+            while (true) {
+                try{
+                    response = new byte[12];
+                    len = port.read(response, 0);
+                    if (len > 0){
+                        mHandler.obtainMessage(MESSAGE_READ, len, -1, response).sendToTarget();
+                    }
+                }
+                catch (IOException e){
+                    Toast toast = Toast.makeText(context, "XXXXXXXXXXX", Toast.LENGTH_SHORT);
+                    toast.show();
+                    break;}
+
+            }
+        }
+
     }
 
     private void startThread() {
         readSerialDataThread = new Thread(new Runnable() {
             public void run() {
-                while (shouldRun) {
-                    int dataSize = 0;
-                    try {
-                        //mOutputStream.write(("DATA_" + System.currentTimeMillis()).getBytes());
-                        Log.e("","1111111111");
-                        dataSize = inStream.available();
-                        if (inStream != null) {
-                            Log.e("", "2222222222");
-                            byte[] data = new byte[dataSize];
-                            inStream.read(data);
-                            message = (new String(data));
-                            Log.e("", message);
-                            if (!message.equals("")) {
-                                display.setText(message);
-                            }
-                        }
-                        Process p = Runtime.getRuntime().exec("su");
-                        DataOutputStream dos = new DataOutputStream(p.getOutputStream());
-                        dos.writeBytes("cd /sys/class/leds/led1\n");
-
-                        if (command.equals("0")) {
-                            command = "1";
-                            dos.writeBytes("echo 1 > brightness\n");
-                        }
-                        else {
-                            command = "0";
-                            dos.writeBytes("echo 0 > brightness\n");}
-                        dos.flush();
-                        dos.close();
-                        p.waitFor();
-                        Thread.sleep(50);
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                byte[] response = new byte[1024];
+                int len;
+                while (true) {
+                    try{
+                        len = port.read(response, 100);
+                        if (len > 0){}
                     }
+                    catch (IOException e){break;}
+
                 }
             }
         });
         readSerialDataThread.start();
+    }
+
+    private void serialMessageHandler(byte[] buffer){
+        switch ((char) buffer[1]){
+            case 'D':
+                Toast.makeText(getApplicationContext(), "DEBUG", Toast.LENGTH_SHORT).show();
+                break;
+
+            case 'L':
+                Toast.makeText(getApplicationContext(), "LED ON", Toast.LENGTH_SHORT).show();
+                break;
+
+            case 'O':
+                int i = (short) ((buffer[3] << 8) | buffer[2]);
+
+                /*ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4);
+
+                byteBuffer.order(ByteOrder.BIG_ENDIAN);
+
+                byteBuffer.put((byte)0x00);
+                byteBuffer.put((byte)0x00);
+                byteBuffer.put((byte)buffer[3]);
+                byteBuffer.put((byte)buffer[2]);
+                byteBuffer.flip();
+                int result = byteBuffer.getInt(); */
+
+                Toast.makeText(getApplicationContext(), "LED OFF " + i, Toast.LENGTH_SHORT).show();
+                break;
+
+            case '1':
+                String s = new String (buffer);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 
 
@@ -176,6 +297,4 @@ public class MainActivity extends Activity {
         catch (Exception e){e.printStackTrace();}
         mSerialPort.close();
     }
-
 }
-
